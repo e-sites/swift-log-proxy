f9bff3282c2fbcf737d491f8a1d905fe60bab0ff
Bas van Kuijck <bvkuijck@unlockagency.nl>
2024-09-20 12:48:58 +0200

    Updated swift 6

--- Sources/ProxyLogHandler/ProxyLogHandler.swift
+++ Sources/ProxyLogHandler/ProxyLogHandler.swift
@@ -1,21 +1,29 @@
 import Foundation
 import Logging
 
-public struct ProxyLogHandler: LogHandler {
+public struct ProxyLogHandler: LogHandler, Sendable {
     public var metadata = Logger.Metadata()
     public var logLevel = Logger.Level.info
     public var label: String
-    private var handler: ((Logger.Level, Logger.Message, Logger.Metadata?, String, String, UInt) -> Void)
-
-    public init(label: String, handler: @escaping ((Logger.Level, Logger.Message, Logger.Metadata?, String, String, UInt) -> Void)) {
+    private var handler: (@Sendable (Logger.Level, Logger.Message, Logger.Metadata?, String, String, String, UInt) -> Void)
+    
+    public init(label: String, handler: @escaping (@Sendable (Logger.Level, Logger.Message, Logger.Metadata?, String, String, String, UInt) -> Void)) {
         self.label = label
         self.handler = handler
     }
-
-    public func log(level: Logger.Level, message: Logger.Message, metadata: Logger.Metadata?, file: String, function: String, line: UInt) {
-        handler(level, message, metadata, file, function, line)
+    
+    public func log( // swiftlint:disable:this function_parameter_count
+        level: Logger.Level,
+        message: Logger.Message,
+        metadata: Logger.Metadata?,
+        source: String,
+        file: String,
+        function: String,
+        line: UInt
+    ) {
+        handler(level, message, metadata, source, file, function, line)
     }
-
+    
     public subscript(metadataKey metadataKey: String) -> Logger.Metadata.Value? {
         get {
             metadata[metadataKey]
